73bd5f70f2ddd90afc7819d79bcc7dc4c2bd3c31
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-05-22 16:32:42 -0300

    Set UserID decorator return type

--- src/auth/userID.decorator.ts
+++ src/auth/userID.decorator.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
+import { ObjectID } from 'typeorm'
 
 export const UserID = createParamDecorator((_, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest()
-  return request.user.userID
+  return request.user.userID as ObjectID
 })
