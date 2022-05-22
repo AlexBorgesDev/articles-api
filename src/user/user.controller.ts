51e7701ba4af7795bc4618f54513c2272c0961b9
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-05-22 14:50:46 -0300

    Made the user creation route public

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,11 +3,14 @@ import { Body, Controller, Post } from '@nestjs/common'
 import { UserCreateDto } from './user.dto'
 import { UserService } from './user.service'
 
+import { Public } from '../auth/public.decorator'
+
 @Controller('user')
 export class UserController {
   constructor(private service: UserService) {}
 
   @Post()
+  @Public()
   async create(@Body() data: UserCreateDto) {
     await this.service.create(data)
     return { message: 'User created successfully' }
