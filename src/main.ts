7c3dc9788a3a80be57d322992e79f7c07ab81b8d
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-05-24 10:17:06 -0300

    Configured the swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 import { PrismaService } from './prisma/prisma.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Articles - API')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, document)
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   app.enableCors()
