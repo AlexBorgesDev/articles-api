dedc95e1126bb91590fb1b31571b679096a7d22f
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-05-25 14:00:55 -0300

    Changed multer local destination path

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -1,7 +1,7 @@
 import * as path from 'path'
 
 export class Paths {
-  static localUploads = path.resolve(__dirname, '..', '..', 'tmp', 'uploads')
+  static localUploads = path.resolve(__dirname, '..', '..', 'public')
 
   static file(filename: string) {
     return path.resolve(this.localUploads, filename)
