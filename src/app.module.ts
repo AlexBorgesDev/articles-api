c44618832432a864768013649e423611f4d839b2
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-05-25 14:08:53 -0300

    Added server static module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common'
 import { APP_GUARD } from '@nestjs/core'
 import { ConfigModule } from '@nestjs/config'
+import { ServeStaticModule } from '@nestjs/serve-static'
+
+import { Paths } from './config/paths'
 
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
@@ -15,6 +18,10 @@ import { PostModule } from './post/post.module'
     AuthModule,
     PictureModule,
     PostModule,
+    ServeStaticModule.forRoot({
+      serveRoot: '/files',
+      rootPath: Paths.localUploads,
+    }),
   ],
   providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
